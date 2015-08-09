18ba09ffc9aa04ed48c95f52c3843b2c8c68b943
Yegor Jbanov <yjbanov@google.com>
2015-08-09 14:00:57 -0700

    transformer can compile text bindings

--- lib/src/transform/template_compiler.dart
+++ lib/src/transform/template_compiler.dart
@@ -7,6 +7,7 @@ class TemplateCompiler {
   final _buf = new StringBuffer();
 
   Node _templateRoot;
+  final _bindings = <Binding>[];
 
   TemplateCompiler(this._componentClassName, this._template);
 
@@ -40,18 +41,49 @@ class TemplateCompiler {
       _emit(' )');
       if (elem.hasChildNodes()) {
         _emit(' (');
-        elem.nodes.forEach(_virtualizeNode);
+        _virtualizeNode(elem.nodes.first);
+        elem.nodes.skip(1).forEach((Node n) {
+          _emit(' ,');
+          _virtualizeNode(n);
+        });
         _emit(' )');
       }
     } else if (node is Text) {
-      _emit(" vText('''${node.text}''')");
+      int textBindingIndex = -1;
+      int currentIndex = 0;
+      final value = new StringBuffer();
+      while((textBindingIndex = node.text.indexOf('{{', textBindingIndex + 1)) >= 0) {
+        int indexOfClosingParens = node.text.indexOf('}}', textBindingIndex + 1);
+        int bindingIndex = int.parse(node.text.substring(textBindingIndex + 2, indexOfClosingParens));
+        value
+            ..write(node.text.substring(currentIndex, textBindingIndex))
+            ..write('\${context.')
+            ..write((_bindings[bindingIndex] as TextBinding).expression)
+            ..write('}');
+        currentIndex = indexOfClosingParens + 2;
+      }
+      value.write(node.text.substring(currentIndex));
+      _emit(" vText('''${value}''')");
     } else {
       throw 'Node type ${node.runtimeType} not yet supported.';
     }
   }
 
+  static final RegExp textBindings = new RegExp(r'\{\{[^(}})]*\}\}');
+
   void _parseTemplate() {
-    _templateRoot = new HtmlParser(_template).parseFragment().children.single;
+    final clean = new StringBuffer();
+    int currentIndex = 0;
+    textBindings.allMatches(_template).forEach((Match m) {
+      String expression = _template.substring(m.start + 2, m.end - 2);
+      _bindings.add(new TextBinding(expression));
+      clean
+        ..write(_template.substring(currentIndex, m.start))
+        ..write('{{${_bindings.length - 1}}}');
+      currentIndex = m.end;
+    });
+    clean.write(_template.substring(currentIndex));
+    _templateRoot = new HtmlParser(clean.toString()).parseFragment().children.single;
   }
 
   void _emitComponentHeader() {
@@ -70,4 +102,11 @@ class TemplateCompiler {
   void _emit(Object o) {
     _buf.write(o);
   }
+}
+
+abstract class Binding {}
+
+class TextBinding implements Binding {
+  final String expression;
+  TextBinding(this.expression);
 }
--- test/src/transform/template_compiler_test.dart
+++ test/src/transform/template_compiler_test.dart
@@ -10,6 +10,35 @@ main() {
       '<div>hello</div>',
       """ vElement('div' ) ( vText('''hello''') )"""
     );
+
+    compileTest(
+      'primitive template with nested nodes',
+      '<div><span>hello</span>, <span>world</span></div>',
+      """ vElement('div' ) ("""
+        """ vElement('span' ) ( vText('''hello''') ) ,"""
+        """ vText(''', ''') ,"""
+        """ vElement('span' ) ( vText('''world''') ) )"""
+    );
+
+    compileTest(
+      'text interpolation',
+      '<div>{{greeting}}</div>',
+      """ vElement('div' ) ( vText('''\${context.greeting}''') )"""
+    );
+
+    compileTest(
+      'multiple text interpolations in same text node',
+      '<div>{{greeting}}, {{person}}</div>',
+      """ vElement('div' ) ( vText('''\${context.greeting}, \${context.person}''') )"""
+    );
+
+    compileTest(
+      'text interpolations in different nodes',
+      '<div><span>{{greeting}},</span><span>{{person}}</span></div>',
+      """ vElement('div' ) ("""
+        """ vElement('span' ) ( vText('''\${context.greeting},''') ) ,"""
+        """ vElement('span' ) ( vText('''\${context.person}''') ) )"""
+    );
   });
 }
 
